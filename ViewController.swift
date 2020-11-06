71f47bbb8a26b6a6654f334543108af61ed4a7b1
Jian Ma <jianquanma@gmail.com>
2020-11-05 20:36:29 -0500

    added model and added urlSEssion call

--- ViewController.swift
+++ ViewController.swift
@@ -7,12 +7,35 @@
 
 import UIKit
 
+struct Course: Decodable {
+    let id: Int
+    let name: String
+    let imageURL: String
+    let number_of_lessons: Int
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fetchCourseJosn { (cources, err) in
+            <#code#>
+        }
     }
+    
+    fileprivate func fetchCourseJosn(completion: @escaping ([Course]?, Error?) -> ()){
+        let urlString = "https://api/letsbuildthatapp.com/jsondecodable/courses"
+        guard let url = URL(string: urlString) else {return}
+        
+        URLSession.shared.dataTask(with: url){ (data, res, err) in
+            if let err = err{
+                completion(nil, err)
+                return
+            }
+        }.resume()
+        
+    }
+    
 
 
 }
