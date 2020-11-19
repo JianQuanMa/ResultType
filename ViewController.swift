df95814e2190769a41b2f0c7b03da478108c143e
Jian Ma <jianquanma@gmail.com>
2020-11-18 20:48:43 -0500

    added in result type that is available since swift 5

--- ViewController.swift
+++ ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 struct Course: Decodable {
     let id: Int
     let name: String
-    let imageURL: String
+    let imageUrl: String
     let number_of_lessons: Int
 }
 
@@ -18,19 +18,46 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        fetchCourseJosn { (cources, err) in
-            <#code#>
+//        fetchCourseJson { (cources, err) in
+//            if let err = err{
+//                print("failed to decode ", err)
+//            return
+//            }
+//            cources?.forEach({ (cource) in
+//                print(cource.name)
+//            })
+//        }
+        fetchCourseJson { (res) in
+            switch res {
+            case .failure(let err):
+                print("we have an err: \(err)")
+            case .success(let cources):
+                cources.forEach { (course) in
+                    print(course.name)
+                }
+            }
         }
+        
     }
     
-    fileprivate func fetchCourseJosn(completion: @escaping ([Course]?, Error?) -> ()){
-        let urlString = "https://api/letsbuildthatapp.com/jsondecodable/courses"
+  //  fileprivate func fetchCourseJson(completion: @escaping ([Course]?, Error?) -> ()){
+    fileprivate func fetchCourseJson(completion: @escaping (Result<[Course], Error>) -> () ){
+        let urlString = "https://api.letsbuildthatapp.com/jsondecodable/courses"
         guard let url = URL(string: urlString) else {return}
         
         URLSession.shared.dataTask(with: url){ (data, res, err) in
             if let err = err{
-                completion(nil, err)
+           //     completion(nil, err)
+                completion(.failure(err))
                 return
+            }
+            do {
+                let cources = try JSONDecoder().decode([Course].self, from: data!)
+               // completion(cources, nil)
+                completion(.success(cources))
+            } catch let jsonErr{
+             //   completion(nil, jsonErr)
+                completion(.failure(jsonErr))
             }
         }.resume()
         
